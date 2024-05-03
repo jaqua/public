192de9e81d131e00cc86aa13f86016f84a045ca8
ashfaq naseem <ashfaqnaseem1@gmail.com>
2024-05-03 14:02:13 +0600

    fixed types, and some formatting

--- apps/project.de/mobile-app/libs/utils.ts
+++ apps/project.de/mobile-app/libs/utils.ts
@@ -1,31 +1,44 @@
-import clsx from "clsx";
-import { Dimensions, PixelRatio } from "react-native";
-type NumOrStr = Number | string;
+import { Dimensions, PixelRatio } from 'react-native'
 
-export const cs = (...inputs: clsx.ClassArray) => {
-  return clsx(...inputs);
-};
+import clsx, { ClassArray } from 'clsx'
+
+type NumOrStr = number | string
+
+export const cs = (...inputs: ClassArray) => {
+  return clsx(...inputs)
+}
+
+const dp = (w: number, percent: number | string) => {
+  const len =
+    typeof percent === 'number' ? percent : parseFloat(percent as string)
+
+  // Use PixelRatio.roundToNearestPixel method in order to round the layout
+  // size (dp) to the nearest one that correspons to an integer number of pixels.
+  return PixelRatio.roundToNearestPixel((w * len) / 100)
+}
 
 export const wp = (widthPercent: NumOrStr) => {
-  const srW = Dimensions.get("window").width;
-  const elemWidth =
-    typeof widthPercent === "number"
-      ? widthPercent
-      : parseFloat(widthPercent as string);
+  const srW = Dimensions.get('window').width
+  return dp(srW, widthPercent)
+  // const elemWidth =
+  //   typeof widthPercent === 'number'
+  //     ? widthPercent
+  //     : parseFloat(widthPercent as string)
 
-  // Use PixelRatio.roundToNearestPixel method in order to round the layout
-  // size (dp) to the nearest one that correspons to an integer number of pixels.
-  return PixelRatio.roundToNearestPixel((srW * elemWidth) / 100);
-};
+  // // Use PixelRatio.roundToNearestPixel method in order to round the layout
+  // // size (dp) to the nearest one that correspons to an integer number of pixels.
+  // return PixelRatio.roundToNearestPixel((srW * elemWidth) / 100)
+}
 
 export const hp = (heightPercent: NumOrStr) => {
-  const srH = Dimensions.get("window").height;
-  const elemHeight =
-    typeof heightPercent === "number"
-      ? heightPercent
-      : parseFloat(heightPercent as string);
+  const srH = Dimensions.get('window').height
+  return dp(srH, heightPercent)
+  // const elemHeight =
+  //   typeof heightPercent === 'number'
+  //     ? heightPercent
+  //     : parseFloat(heightPercent as string)
 
-  // Use PixelRatio.roundToNearestPixel method in order to round the layout
-  // size (dp) to the nearest one that correspons to an integer number of pixels.
-  return PixelRatio.roundToNearestPixel((srH * elemHeight) / 100);
-};
+  // // Use PixelRatio.roundToNearestPixel method in order to round the layout
+  // // size (dp) to the nearest one that correspons to an integer number of pixels.
+  // return PixelRatio.roundToNearestPixel((srH * elemHeight) / 100)
+}
